07cbfdb534dd8ee00de253bde83a376e28f0d3a3
Leonardo Bonetti <Leonardo.bonetti@etec.sp.gov.br>
2019-09-25 00:26:41 -0300

    Update do script SQL para geração das tabelas

--- sqldb_create.sql
+++ sqldb_create.sql
@@ -57,8 +57,8 @@ CREATE TABLE IF NOT EXISTS `truckpad_db`.`Drivers` (
   `OwnVehicle` TINYINT NOT NULL,
   `InsertDate` DATETIME NULL,
   PRIMARY KEY (`ID`),
-  INDEX `FK_CNHType_idx` (`FK_CNHTypes` ASC) VISIBLE,
-  INDEX `FK_Gender_idx` (`FK_Gender` ASC) VISIBLE,
+  INDEX `FK_CNHType_idx` (`FK_CNHTypes` ASC) ,
+  INDEX `FK_Gender_idx` (`FK_Gender` ASC) ,
   CONSTRAINT `FK_CNHTypes`
     FOREIGN KEY (`FK_CNHTypes`)
     REFERENCES `truckpad_db`.`CNHTypes` (`ID`)
@@ -118,10 +118,10 @@ CREATE TABLE IF NOT EXISTS `truckpad_db`.`Itineraries` (
   `FK_Dest_Addresses` INT NULL,
   `FK_Origin_Addresses` INT NULL,
   PRIMARY KEY (`ID`),
-  INDEX `FK_Driver_idx` (`FK_Drivers` ASC) VISIBLE,
-  INDEX `FK_TruckType_idx` (`FK_TruckType` ASC) VISIBLE,
-  INDEX `FK_Origin_Addresses` (`FK_Dest_Addresses` ASC) VISIBLE,
-  INDEX `FK_Dest_Addresses` (`FK_Origin_Addresses` ASC) VISIBLE,
+  INDEX `FK_Driver_idx` (`FK_Drivers` ASC) ,
+  INDEX `FK_TruckType_idx` (`FK_TruckType` ASC) ,
+  INDEX `FK_Origin_Addresses` (`FK_Dest_Addresses` ASC) ,
+  INDEX `FK_Dest_Addresses` (`FK_Origin_Addresses` ASC) ,
   CONSTRAINT `FK_Drivers`
     FOREIGN KEY (`FK_Drivers`)
     REFERENCES `truckpad_db`.`Drivers` (`ID`)
@@ -201,4 +201,135 @@ INSERT INTO `truckpad_db`.`TruckTypes` (`ID`, `Description`) VALUES (4, 'Carreta
 INSERT INTO `truckpad_db`.`TruckTypes` (`ID`, `Description`) VALUES (5, 'Carreta Eixo Extendido');
 
 COMMIT;
+
+
+DELIMITER $$
+CREATE DEFINER=`admin`@`%` PROCEDURE `GetDrivers`(
+
+	IN ID INT(11),
+    IN OwnVehicle bool
+
+)
+BEGIN
+	SELECT * FROM Drivers WHERE
+    (ID IS NULL OR ID = Drivers.ID) AND
+    (OwnVehicle IS NULL OR OwnVehicle = Drivers.OwnVehicle);
+END;$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE DEFINER=`admin`@`%` PROCEDURE `GetItineraries`(
+	IN InitialLoadPeriod DATETIME,
+    IN FinalLoadPeriod DATETIME,
+    IN FK_TruckType INT(11),
+    IN Loaded bool,
+    IN Finished bool,
+    IN OAState VARCHAR(256),
+    IN OACity VARCHAR(256),
+	IN DAState VARCHAR(256),
+    IN DACity VARCHAR(256),
+    IN ID int(11)
+)
+BEGIN
+SELECT I.ID IDItinerarie, D.ID IDDriver, CONCAT(D.Name, ' ', D.Lastname) DriverName,TT.ID TruckTypeID, I.Loaded,
+    TT.Description TruckTypeDescription, I.Finished, I.LoadDateTime, I.UnLoadDateTime,
+    OA.ID as OrigAdressID,OA.Lng OrigLongitude, OA.Lat OrigLatitude, OA.Address OrigAddress, OA.StreetNumber OrigStreetNumber, OA.City OrigCity, OA.State OrigState,
+    DA.ID as DestAdressID,DA.Lng DestLongitude, DA.Lat DestLatitude, DA.Address DestAddress, DA.StreetNumber DestStreetNumber , DA.City DestCity, DA.State DestState
+
+    FROM DRIVERS D
+    inner join ITINERARIES I on I.FK_DRIVERS = D.ID
+    inner join Addresses OA on OA.ID = I.FK_Dest_Addresses
+    inner join Addresses DA on DA.ID = I.FK_Origin_Addresses
+    inner join trucktypes TT on TT.ID = I.FK_TruckType
+    WHERE
+		(Finished IS NULL OR I.Finished = Finished) AND
+		(Loaded IS NULL OR I.Loaded = Loaded) AND
+        (InitialLoadPeriod IS NULL OR I.LoadDateTime >= InitialLoadPeriod ) AND
+        (FinalLoadPeriod IS NULL OR I.LoadDateTime <= FinalLoadPeriod) AND
+        (FK_TruckType IS NULL OR I.FK_TruckType = FK_TruckType) AND
+        (OAState IS NULL OR OA.State = OAState) AND
+        (OACity IS NULL OR OA.City = OACity) AND
+        (DAState IS NULL OR DA.State = DAState) AND
+        (DACity IS NULL OR DA.City = DACity) AND
+        (ID IS NULL OR I.ID = ID);
+END;$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE DEFINER=`admin`@`%` PROCEDURE `GetItinerariesGroupedByDay`(
+	IN InitialLoadPeriod DATETIME,
+    IN FinalLoadPeriod DATETIME,
+    IN Loaded BOOL
+)
+BEGIN
+	IF(InitialLoadPeriod IS NULL  OR FinalLoadPeriod IS NULL)
+    THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'NULL is not allowed.';
+	END IF;
+
+SELECT DATE(I.LoadDateTime) AS Period,
+       COUNT(*) AS Itineraries
+  FROM Itineraries I
+  WHERE
+	(InitialLoadPeriod IS NULL OR I.LoadDateTime >= InitialLoadPeriod ) AND
+	(FinalLoadPeriod IS NULL OR I.LoadDateTime <= FinalLoadPeriod) AND
+    (Loaded IS NULL OR I.Loaded = Loaded)
+ GROUP BY DATE(I.LoadDateTime)
+ ORDER BY DATE(I.LoadDateTime);
+END;$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE DEFINER=`admin`@`%` PROCEDURE `GetItinerariesGroupedByMonth`(
+	IN InitialLoadPeriod DATETIME,
+    IN FinalLoadPeriod DATETIME,
+    IN Loaded BOOL
+)
+BEGIN
+	IF(InitialLoadPeriod IS NULL  OR FinalLoadPeriod IS NULL)
+    THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'NULL is not allowed.';
+	END IF;
+
+SELECT DATE_FORMAT(I.LoadDateTime, '%Y-%m') AS Period,
+       COUNT(*) AS Itineraries
+  FROM Itineraries I
+  WHERE
+	(InitialLoadPeriod IS NULL OR I.LoadDateTime >= InitialLoadPeriod ) AND
+	(FinalLoadPeriod IS NULL OR I.LoadDateTime <= FinalLoadPeriod) AND
+    (Loaded IS NULL OR I.Loaded = Loaded)
+ GROUP BY MONTH(DATE(LoadDateTime)),YEAR(DATE(LoadDateTime))
+ ORDER BY YEAR(DATE(LoadDateTime)),MONTH(DATE(LoadDateTime));
+END;$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`admin`@`%` PROCEDURE `GetItinerariesGroupedByYear`(
+	IN InitialLoadPeriod DATETIME,
+    IN FinalLoadPeriod DATETIME,
+    IN Loaded BOOL
+)
+BEGIN
+	IF(InitialLoadPeriod IS NULL  OR FinalLoadPeriod IS NULL)
+    THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'NULL is not allowed.';
+	END IF;
+
+SELECT DATE_FORMAT(I.LoadDateTime, '%Y') AS Period,
+       COUNT(*) AS Itineraries
+  FROM Itineraries I
+
+  WHERE
+	(InitialLoadPeriod IS NULL OR I.LoadDateTime >= InitialLoadPeriod) AND
+	(FinalLoadPeriod IS NULL OR I.LoadDateTime <= FinalLoadPeriod) AND
+    (Loaded IS NULL OR I.Loaded = Loaded)
+ GROUP BY YEAR(DATE(LoadDateTime))
+ ORDER BY YEAR(DATE(LoadDateTime));
+END;$$
+DELIMITER ;
+
+
 
